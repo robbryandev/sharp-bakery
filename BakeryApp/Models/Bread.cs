cee81dfa95d7409f3d4c4d34a8be331448908b77
robbryandev <robbryandev@gmail.com>
2022-12-09 09:12:18 -0800

    Add Bread.GetPrice definition

--- BakeryApp/Models/Bread.cs
+++ BakeryApp/Models/Bread.cs
@@ -1,8 +1,10 @@
 namespace BakeryApp {
   public class Bread {
-    public float price {get;} = 5;
+    public static float price {get;} = 5;
     public static string GetPrice(int amount) {
-      return "";
+      int dealAmount = (int)(amount / 3);
+      int resultPrice = (int)((amount - dealAmount) * price);
+      return String.Format("${0}", resultPrice);
     }
   }
 }
